722bb4840584d1ba626dade29ccdfa470b6d23bf
oijoijivoijl2298 <91876461+oijoijivoijl2298@users.noreply.github.com>
2021-10-04 10:51:20 +0200

    Add files via upload
    game project week one

---
+++ wk01-09projectguessthenumbergamebyEdwin.py
@@ -0,0 +1,17 @@
+import random
+nummer = random.randint(1,100)
+win = False
+beurten =0
+while win==False:
+    Your_guess = int(input("Enter a number between 1 and 100"))
+    beurten +=1
+    if nummer==Your_guess:
+        print("good boy you done right today!")
+        print("score: aantal beurten gehad: ",beurten)
+        win == True
+        break
+    else:
+     if nummer>Your_guess:
+        print("Your Guess was low, Please enter a higher number")
+     else:
+        print("your guess was high, please enter a lower number")
